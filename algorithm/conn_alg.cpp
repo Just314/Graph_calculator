c8a3c3e9b3b6d0def9537c8dd68b9edd91ea4573
Riemann <daniiarov_a@auca.kg>
2020-05-02 18:40:44 +0600

    Almost completely realized connectedness search. Issues with extreme cases (and probably loops) are to be fixed. Added examples.

--- algorithm/conn_alg.cpp
+++ algorithm/conn_alg.cpp
@@ -1,29 +1,47 @@
 #include "conn_alg.h"
 using namespace std; 
 
-void Connectedness_check::Depth_Fist_Search(u_short s) {
-    if (visited[s]) return;
-    counter++;
-    vector <u_short> neighbors = g->searchVertexConnectionsOut(s);
-    visited[s] = true;
-    for (auto u: neighbors) {
-    	Depth_Fist_Search(u);
+
+void Connectedness_check::Depth_Fist_SearchOut(u_short s) {
+    if (visitedOut[s]) return;
+    counterOut++;
+    vector <u_short> neighborsout = g->searchVertexConnectionsOut(s);
+    visitedOut[s] = true;
+    for (auto u: neighborsout) {
+    	Depth_Fist_SearchOut(u);
+    }
+}
+
+void Connectedness_check::Depth_Fist_SearchIn(u_short s) {
+    if (visitedIn[s]) return;
+    counterIn++;
+    vector <u_short> neighborsin = g->searchVertexConnectionsIn(s);
+    visitedIn[s] = true;
+    for (auto u: neighborsin) {
+    	Depth_Fist_SearchIn(u);
     }
 }
 
 void Connectedness_check::Initialize(Graph* g_in){
     this->g = g_in;
     cardinal =  g->getVertCount();
-    counter = 0;
+    counterOut = 0;
+    counterIn = 0;
 }
 
 string Connectedness_check::Calculate() {
     string Is_Connected;
-    visited.assign(cardinal+1, false);
-    Depth_Fist_Search(1);
-
-    if (counter == cardinal) {
-        Is_Connected = "Graph is connected.\n";
+    visitedOut.assign(cardinal+1, false);
+    visitedIn.assign(cardinal+1, false);
+    Depth_Fist_SearchOut(1);
+    Depth_Fist_SearchIn(1);
+    if (counterOut == cardinal || counterIn == cardinal) {
+        weakly_connected = true;        
+        Is_Connected = "Graph is weakly connected.\n";
+        if (counterIn == counterOut) {
+            strongly_connected = true;
+            Is_Connected = "Graph is connected.\n";
+        }
     }
     else {
         Is_Connected = "Graph is not connected.\n";
--- algorithm/conn_alg.h
+++ algorithm/conn_alg.h
@@ -7,13 +7,21 @@ class Connectedness_check : public IAlg{
     private:
         Graph *g;
         u_short cardinal;
-        std::vector<bool> visited;
-        u_short counter;
+        std::vector<bool> visitedOut;
+        u_short counterOut;
+        std::vector<bool> visitedIn;
+        u_short counterIn;
+        bool strongly_connected;
+        bool weakly_connected;
+        
     public:
         Connectedness_check(){}
         std::string Calculate();
         void Initialize(Graph* g);
-        void Depth_Fist_Search(u_short s);
+        void Depth_Fist_SearchOut(u_short s);
+        void Depth_Fist_SearchIn(u_short s);
+        bool Is_Strongly_Connected() {return strongly_connected;}
+        bool Is_Weakly_Connected() {return weakly_connected;}
 };
 
 
